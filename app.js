5287b8a64b66ddbc621bf7f2ea037a83b7e6b461
BryanQuintanilla <qa0340032023@unab.edu.sv>
2026-02-19 16:26:59 -0600

    func: Metodo para crear producto .

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ let productos = [
   { id: 2, nombre: "Mouse Logitech", descripcion: "Inalámbrico ergonómico", precio: 45, stock: 15, disponible: true },
   { id: 3, nombre: "Teclado Mecánico", descripcion: "Switches azules RGB", precio: 90, stock: 0, disponible: false },
 ];
+
+let nextId = 4;
+
+// ── CREAR / ACTUALIZAR ──────────────────────────────────────────
+function guardar() {
+  const nombre      = document.getElementById("inp-nombre").value.trim();
+  const descripcion = document.getElementById("inp-descripcion").value.trim();
+  const precio      = parseFloat(document.getElementById("inp-precio").value);
+  const stock       = parseInt(document.getElementById("inp-stock").value);
+  const disponible  = document.getElementById("inp-disponible").checked;
+
+  productos.push({ id: nextId++, nombre, descripcion, precio, stock, disponible });
+
+}
